a40244f0b6984c87d9b2ad713add609ec5be0786
SKYDEFREAM <skydream855@gmail.com>
2022-03-25 12:38:40 +0300

    Ball

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public Ball tryRun(boolean condition) {
+        if (condition == true) {
+            System.out.println("tryEat");
+            } else {
+            System.out.println("tryRun");
+        }
+        return null;
+    }
+}
