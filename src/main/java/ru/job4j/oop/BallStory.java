60ae51dd0c557572717b1a729cf7535b8704e756
SKYDEFREAM <skydream855@gmail.com>
2022-03-26 15:55:44 +0300

    Error

--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -6,8 +6,8 @@ public class BallStory  {
         Hare hare = new Hare();
         Wolf wolf = new Wolf();
         Ball ball = new Ball();
-        fox.tryEat(ball.tryRun(true));
-        hare.tryEat(ball.tryRun(false));
-        wolf.tryEat(ball.tryRun(false));
+        fox.tryEat(ball);
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
     }
 }
