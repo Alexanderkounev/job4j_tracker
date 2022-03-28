41016807cb56cf836b741e54f764e374058f40a1
SKYDEFREAM <skydream855@gmail.com>
2022-03-28 15:42:46 +0300

    Ball

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -1,12 +1,11 @@
 package ru.job4j.oop;
 
-public class Ball  {
-    public Ball tryRun(boolean condition) {
+public class Ball {
+    public void tryRun(boolean condition) {
         if (condition) {
             System.out.println("tryEat");
             } else {
             System.out.println("tryRun");
         }
-        return null;
     }
 }
--- src/main/java/ru/job4j/oop/BallStory.java
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -1,6 +1,6 @@
 package ru.job4j.oop;
 
-public class BallStory {
+public class BallStory  {
     public static void main(String[] args) {
         Fox fox = new Fox();
         Hare hare = new Hare();
