4e86c99a802424ccd8acf5af2b9f514d935c1a49
SKYDEFREAM <skydream855@gmail.com>
2022-04-04 23:14:30 +0300

    Book + Library

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book = new Book("Clean code", 35);
+        Book book1 = new Book("Helicopter", 56);
+        Book book2 = new Book("Car Price", 32);
+        Book book3 = new Book("Java", 562);
+        Book[] books = new Book[4];
+        books[0] = book;
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getNumberOfPages() + " pages.");
+        }
+        System.out.println();
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (Book bk : books) {
+            System.out.println(bk.getName() + " - " + bk.getNumberOfPages() + " pages.");
+        }
+        System.out.println();
+        for (Book bk : books) {
+            if (bk.equals(book)) {
+                System.out.println(bk.getName() + " - " + bk.getNumberOfPages() + " pages.");
+            }
+        }
+    }
+}
